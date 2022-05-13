adce6a57d135960adfd8af2ad60363f4e621b48e
KARTHIK N <59639795+KARTHIK-N-94496@users.noreply.github.com>
2022-05-14 01:08:10 +0530

    Project Created with Spring Starter Dependencies added: Lombok, Spring Web Commit on 14-05-2022

---
+++ src/main/java/com/SpringBootProject/CoinsRankingApp_RedisStackWithSpringBoot/CoinsRankingAppRedisStackWithSpringBootApplication.java
@@ -0,0 +1,21 @@
+package com.SpringBootProject.CoinsRankingApp_RedisStackWithSpringBoot;
+
+/*
+Changing git username and email for VCS
+	$ git config user.name // Returns username
+	$ git config user.email // Returns user email
+	$ git config --global user.name "USER NAME" // Changes to USER NAME
+	$ git config --global user.email "102715150+Gilfoyle24x7@users.noreply.github.com" // No reply email will be available in Github->Settings->Emails->Copy is available or add an email address to get it
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinsRankingAppRedisStackWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinsRankingAppRedisStackWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBootProject/CoinsRankingApp_RedisStackWithSpringBoot/CoinsRankingAppRedisStackWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootProject.CoinsRankingApp_RedisStackWithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinsRankingAppRedisStackWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
